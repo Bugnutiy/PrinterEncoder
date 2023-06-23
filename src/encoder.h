5b1e878f6be9ce76473c3d9a8950e912e6dd0e1b
Bugnutiy <bigbag.minecrafter@gmail.com>
2023-06-23 22:39:52 +0300

    tst

--- src/encoder.h
+++ src/encoder.h
@@ -1,2 +1,37 @@
 #pragma once
 #include <Arduino.h>
+
+#ifndef PIN_ENC_A
+#define PIN_ENC_A 2 // вход А
+#endif
+#ifndef PIN_ENC_B
+#define PIN_ENC_B 3 // вход Б
+#endif
+
+#ifndef ENC_READ
+#define ENC_READ(PIN) (PIND & (1 << PIN))
+#endif
+class Encoder
+{
+private:
+    /* data */
+public:
+    int32_t enc_pos = 0;
+    Encoder();
+    ~Encoder();
+    static void encInterrupt(void);
+};
+
+Encoder::Encoder()
+{
+}
+
+Encoder::~Encoder()
+{
+}
+
+void Encoder::encInterrupt()
+{
+    bool A = ENC_READ(PIN_ENC_A);
+    bool B = ENC_READ(PIN_ENC_B);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include <Arduino.h>
+#define PIN_ENC_A 2 // вход А
+#define PIN_ENC_B 3 // вход Б
+
+#define DEBUG 9600 // скорость монитора порта. Или 0 для отключения
+#if DEBUG
+#define DD(x) Serial.print(x)
+#define DDD(x) Serial.println(x)
+#else
+#define DD(x)
+#define DDD(x)
+#endif
 #include "encoder.h"
 
-void setup(){
-
+Encoder encoder;
+void setup()
+{
+#if DEBUG
+    Serial.begin(DEBUG);
+#endif
+attachInterrupt(0,*&encoder.encInterrupt(),CHANGE);
 }
 
-void loop(){
-    
+void loop()
+{
 }
